070dfda5cdeb3a9f4b149cbd383a81196b90dbd7
Thomas Drevon <thomas@drevon.no>
2022-01-31 14:57:33 +0100

    Update main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -1,54 +1,30 @@
 function handleStop () {
-    basic.showLeds(`
-        # . . . #
-        . # . # .
-        . . # . .
-        . # . # .
-        # . . . #
-        `)
+    bitbot.setLedColor(0xFF0000)
     bitbot.stop(BBStopMode.Coast)
 }
 function handleForward (num: number) {
     forward = num / 10
     if (forward > 0) {
-        basic.showLeds(`
-            . . # . .
-            . # # # .
-            # . # . #
-            . . # . .
-            . . # . .
-            `)
+        bitbot.setPixelColor(5, 0x00FF00)
+        bitbot.setPixelColor(11, 0x00FF00)
         bitbot.go(BBDirection.Forward, forward)
     } else {
-        basic.showLeds(`
-            . . # . .
-            . . # . .
-            # . # . #
-            . # # # .
-            . . # . .
-            `)
+        bitbot.setPixelColor(0, 0xFF0000)
+        bitbot.setPixelColor(6, 0xFF0000)
         bitbot.go(BBDirection.Reverse, Math.abs(forward))
     }
 }
 function handleTurn (num: number) {
     turn = num / 10
     if (turn > 0) {
-        basic.showLeds(`
-            . . # . .
-            . # . . .
-            # # # # #
-            . # . . .
-            . . # . .
-            `)
+        for (let index = 0; index <= 5; index++) {
+            bitbot.setPixelColor(index + 6, 0xFFFF00)
+        }
         bitbot.rotate(BBRobotDirection.Right, turn)
     } else {
-        basic.showLeds(`
-            . . # . .
-            . . . # .
-            # # # # #
-            . . . # .
-            . . # . .
-            `)
+        for (let index = 0; index <= 5; index++) {
+            bitbot.setPixelColor(index, 0xFFFF00)
+        }
         bitbot.rotate(BBRobotDirection.Left, Math.abs(turn))
     }
 }
@@ -56,11 +32,10 @@ radio.onReceivedValue(function (name, value) {
     if (name.includes("stop")) {
         handleStop()
     }
-    if (Math.abs(value) < 200) {
-        basic.showIcon(IconNames.Happy)
-    } else {
+    if (Math.abs(value) > 200) {
+        bitbot.ledClear()
         if (name.includes("forward")) {
-            handleForward(1)
+            handleForward(forward)
         }
         if (name.includes("turn")) {
             handleTurn(value)
@@ -70,3 +45,5 @@ radio.onReceivedValue(function (name, value) {
 let turn = 0
 let forward = 0
 radio.setGroup(1)
+basic.showIcon(IconNames.Happy)
+bitbot.setLedColor(0x00FF00)
